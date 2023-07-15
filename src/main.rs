0ae4c25fdc298feaed6c1bea93c7cd967557ee5d
Jason Costello <jason@hazy.com>
2023-07-15 21:38:10 +0100

    Single threaded diddy server working

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use std::io::{BufRead, BufReader};
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "0.0.0.0";
 const PORT: &str = "8081";
 
 fn main() {
+    println!("Server is up...");
     let addr = format!("{}:{}", HOST, PORT);
     let listener = TcpListener::bind(addr).unwrap();
 
-    let mut count = 0;
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
-        count += 1;
-
-        println!("Total no. requests: {}", count)
     }
 }
 
@@ -23,8 +21,37 @@ fn handle_connection(mut stream: TcpStream) {
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
+        .take(3)
         .collect();
 
-    println!("Request: {:#?}", http_request)
+    let http_version = "HTTP/1.1";
+    let ok = "200 OK";
+    let not_found = "404 NOT FOUND";
+
+    let request_path = http_request
+        .first()
+        .unwrap()
+        .strip_suffix(" HTTP/1.1")
+        .unwrap();
+
+    let (status, doc) =
+        if request_path == "GET /" {
+            (format!("{http_version} {ok}"), "assets/index.html")
+        } else {
+            (format!("{http_version} {not_found}"), "assets/404.html")
+        };
+
+    let contents = fs::read_to_string(doc).unwrap();
+    let length = contents.len();
+
+    println!("{:#?}", http_request);
+
+
+    let response = format!(
+        "{status}\r\n\
+         Content-Length: {length}\r\n\r\n\
+         {contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
